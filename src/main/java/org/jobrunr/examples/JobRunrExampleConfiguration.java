9ebdee0e68f711c91133591be1289def5600d9be
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2021-06-02 15:56:12 +0200

    Update to latest version

--- src/main/java/org/jobrunr/examples/JobRunrExampleConfiguration.java
+++ src/main/java/org/jobrunr/examples/JobRunrExampleConfiguration.java
@@ -4,15 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.sqlite.SQLiteDataSource;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
 public class JobRunrExampleConfiguration {
 
     @Bean
-    public SQLiteDataSource dataSource() {
+    public SQLiteDataSource dataSource() throws IOException {
+        final Path path = Paths.get(System.getProperty("java.io.tmpdir"), "jobrunr-example.db");
+        Files.delete(path);
         final SQLiteDataSource dataSource = new SQLiteDataSource();
-        dataSource.setUrl("jdbc:sqlite:" + Paths.get(System.getProperty("java.io.tmpdir"), "jobrunr-example.db"));
+        dataSource.setUrl("jdbc:sqlite:" + path);
         return dataSource;
     }
 }
