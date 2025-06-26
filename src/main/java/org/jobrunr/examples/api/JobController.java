ba7428c8bc5bd6381233f7646890fe5c3f4ed9ad
rdehuyss <ronald.dehuysser@gmail.com>
2025-06-26 13:53:57 +0200

    Test JobRunr v7

--- src/main/java/org/jobrunr/examples/api/JobController.java
+++ src/main/java/org/jobrunr/examples/api/JobController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
 import java.util.UUID;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.time.Instant.now;
+import static org.jobrunr.scheduling.JobBuilder.aJob;
 
 @RestController
 public class JobController {
+
+    private static String HIGH_PRIO_QUEUE = "HighPrio";
 
     private final JobScheduler jobScheduler;
     private final SampleJobService sampleService;
@@ -47,5 +52,23 @@ public class JobController {
             @RequestParam(value = "when", defaultValue = "PT3H") String when) {
         final JobId scheduledJobId = jobScheduler.schedule(now().plus(Duration.parse(when)), () -> sampleService.executeSampleJob("Hello " + name));
         return "Job Scheduled: " + scheduledJobId.toString();
+    }
+
+    @GetMapping("/enqueue-rate-limited-jobs")
+    public String enqueueRateLimitedJobs(@RequestParam(value = "name", defaultValue = "World") String name) {
+        int totalAmount = 1000;
+        Stream<Integer> items = IntStream.range(0, totalAmount).boxed();
+        jobScheduler.enqueue(items, x -> sampleService.executeRateLimitedJob("Hello " + name));
+        return "Enqueued " + totalAmount + " rate limited job";
+    }
+
+    @GetMapping("/enqueue-job-on-dynamic-queue")
+    public String enqueueDynamicQueueJob(@RequestParam(value = "tenant") String tenant) {
+        final JobId enqueuedJobId = jobScheduler.create(aJob()
+                .withLabels("tenant: " + tenant)
+                .withQueue(HIGH_PRIO_QUEUE)
+                .withDeleteOnSuccess(Duration.ofHours(2), Duration.ofMinutes(5))
+                .withDetails(x -> sampleService.executeJobForTenant(tenant)));
+        return "Job Enqueued for tenant '" + tenant + "': " + enqueuedJobId.toString();
     }
 }
--- src/main/java/org/jobrunr/examples/services/SampleJobService.java
+++ src/main/java/org/jobrunr/examples/services/SampleJobService.java
@@ -38,9 +38,34 @@ public class SampleJobService {
         }
     }
 
+    @Job(name = "The rate limited job with variable %0", retries = 2, rateLimiter = "my-rate-limiter")
+    public void executeRateLimitedJob(String input) throws InterruptedException {
+        logger.info("The sample job has begun. The variable you passed is {}", input);
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            logger.error("Error while executing sample job", e);
+            throw e;
+        } finally {
+            logger.info("Sample job has finished...");
+        }
+    }
+
     @Job
     public void sampleJobWithRecordInput(SampleJobInput sampleJobInput) throws InterruptedException {
         logger.info("The sample job has begun. The variable you passed is {}", sampleJobInput.id());
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            logger.error("Error while executing sample job", e);
+            throw e;
+        } finally {
+            logger.info("Sample job has finished...");
+        }
+    }
+
+    public void executeJobForTenant(String tenant) throws InterruptedException {
+        logger.info("The sample job for tenant {} has begun.", tenant);
         try {
             Thread.sleep(1500);
         } catch (InterruptedException e) {
